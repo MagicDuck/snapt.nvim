59edab131669dca52a8f2958336165bb9758f38c
Stephan Badragan <stephan.badragan@everbridge.com>
2025-07-30 22:13:08 -0700

    adding emmylua support

--- spec/test_spec.lua
+++ spec/test_spec.lua
@@ -1,5 +1,7 @@
 ---@type luassert
 local assert = require('luassert')
+-- TODO (sbadragan): in order for things to work, we need to return stuff from our helper,
+-- and
 
 describe('my test spec', function()
   it('shuld run a simple test', function()
