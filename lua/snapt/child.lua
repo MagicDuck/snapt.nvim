c77795b4ae717227a2a3f8de638b189d0ac76790
Stephan Badragan <stephan.badragan@everbridge.com>
2025-10-03 14:06:40 -0700

    moving to differnt impl

--- lua/snapt/child.lua
+++ lua/snapt/child.lua
@@ -1,69 +1,25 @@
+-- TODO (sbadragan): rename this file
 -- NOTE: the following code takes heavy inspiration from mini.nvim test module
 -- nvim child process implementation: https://github.com/nvim-mini/mini.nvim/blob/main/lua/mini/test.lua
----@class snapt.NvimInstance
-local NvimInstance = {}
-NvimInstance.__index = NvimInstance
-
--- trick language server to have proper types those get overriden in the constructor
--------------------------------------------------------------------------------------------
-
---- Sugar for `vim.api.xxx` API to be executed inside nvim instance
-NvimInstance.api = vim.api
-
---- Variant of `api` functions called with `vim.rpcnotify`. Useful for making
---- blocking requests (like `getcharstr()`).
-NvimInstance.api_notify = vim.api
-
--------------------------------------------------------------------------------------------
-
--- TODO (sbadragan): what do we want in here?
--- do we want the full API?? or just some child.lua(), child.lua_get()
--- usage:
--- local c = NvimInstance.new()
--- c.start()
--- c.stop()
--- or:
--- local c = snapt.get_nvim_instance(args) -> returns c or errors
--- c.stop()
+-- There's really not a lot of ways you can write it and mini.test already did a cracking job on most things IMO.
+-- Credits go to mini.test's author and mistakes in this re-working are mine :)
 
 ---@class snapt.NvimInstanceOpts
----@field nvim_executable? string
+---@field nvim_executable? string path to nvim executable
 ---@field nvim_args? string[] additional neovim arguments
 ---@field connection_timeout? integer
 
---- Returns an object representing a nvim instance
----@param opts? snapt.NvimInstanceOpts
-function NvimInstance.new(opts)
-  local self = setmetatable({}, NvimInstance)
-  self.opts = vim.tbl_deep_extend(
-    'force',
-    { nvim_executable = vim.v.progpath, nvim_args = {}, connection_timeout = 5000 },
-    opts or {}
-  )
-
-  self.api = setmetatable({}, {
-    __index = function(_, key)
-      return function(...)
-        return vim.rpcrequest(self.job.channel, key, ...)
-      end
-    end,
-  })
-
-  self.api_notify = setmetatable({}, {
-    __index = function(_, key)
-      return function(...)
-        return vim.rpcnotify(self.job.channel, key, ...)
-      end
-    end,
-  })
-
-  self:_start()
-  return self
-end
+---@class snapt.NvimInstaceJob
+---@field address string
+---@field id integer
+---@field channel integer
 
 --- starts nvim child process
----@private
-function NvimInstance:_start()
+---@param nvim_executable string
+---@param nvim_args string[]
+---@param connection_timeout integer
+---@return snapt.NvimInstaceJob
+function start_nvim_instance(nvim_executable, nvim_args, connection_timeout)
   -- Make unique name for `--listen` pipe
   local job = { address = vim.fn.tempname() }
 
@@ -75,13 +31,13 @@ function NvimInstance:_start()
 
   --stylua: ignore
   local full_args = {
-    self.opts.nvim_executable, '--clean', '-n', '--listen', job.address,
+    nvim_executable, '--clean', '-n', '--listen', job.address,
     -- Setting 'lines' and 'columns' makes headless process more like
     -- interactive for closer to reality testing
     -- TODO (sbadragan): pass lines and columns as options
     '--headless', '--cmd', 'set lines=24 columns=80'
   }
-  vim.list_extend(full_args, args)
+  vim.list_extend(full_args, nvim_args)
 
   -- Using 'jobstart' for creating a job is crucial for getting this to work
   -- in Github Actions. Other approaches:
@@ -94,7 +50,7 @@ function NvimInstance:_start()
   local step = 10
   local connected = nil
   local i = 0
-  local max_tries = math.floor(self.opts.connection_timeout / step)
+  local max_tries = math.floor(connection_timeout / step)
   local channel_or_err = nil
   repeat
     i = i + 1
@@ -108,54 +64,133 @@ function NvimInstance:_start()
   end
 
   job.channel = channel_or_err --[[@as integer]]
-  self.job = job
+  return job
 end
 
-function NvimInstance:stop()
-  -- Properly exit Neovim. `pcall` avoids `channel closed by client` error.
-  -- Also wait for it to actually close. This reduces simultaneously opened
-  -- Neovim instances and CPU load (overall reducing flakey tests).
-  -- TODO (sbadragan): should this be self:cmd ??
-  pcall(NvimInstance.cmd, self, 'silent! 0cquit')
-  vim.fn.jobwait({ self.job.id }, 1000)
+---@param options snapt.NvimInstanceOpts
+function create_nvim_instance(options)
+  local inst = {}
 
-  -- Close all used channels. Prevents `too many open files` type of errors.
-  pcall(vim.fn.chanclose, self.job.channel)
-  pcall(vim.fn.chanclose, self.job.id)
+  local opts = vim.tbl_deep_extend(
+    'force',
+    { nvim_executable = vim.v.progpath, nvim_args = {}, connection_timeout = 5000 },
+    options or {}
+  )
 
-  -- Remove file for address to reduce chance of "can't open file" errors, as
-  -- address uses temporary unique files
-  pcall(vim.fn.delete, self.job.address)
-end
+  local state = {
+    job = start_nvim_instance(opts.nvim_executable, opts.nvim_args, opts.connection_timeout) --[[@as snapt.NvimInstaceJob?]],
+  }
 
---- check if instance is blocked
---- i.e. it waits for user input and won't return from other call. Common causes are
---- active |hit-enter-prompt| (can mitigate by increasing prompt height to a bigger value)
---- or Operator-pending mode (can mitigate by exiting it)
----@return boolean
-function NvimInstance:is_blocked()
-  return self.api.nvim_get_mode()['blocking']
-end
+  function ensure_job_started()
+    if not state.job then
+      error('Canot perform action! Neovim instance is stopped!')
+    end
 
--- TODO (sbadragan): not sure about this....
--- should we call it on every command?
--- should it error out in middle?
--- should we change error message?
-function NvimInstance:_prevent_hanging()
-  if not self:is_blocked() then
-    return
+    return state.job --[[@as snapt.NvimInstaceJob]]
   end
 
-  -- TODO (sbadragan): better explanation
-  error(string.format('NvimInstance: process is blocked.'))
-end
+  --- stop neovim instance
+  inst.stop = function()
+    local job = state.job
+    if not job then
+      return
+    end
 
---- runs vimscript in the context of the instance
----@param cmd string
-function NvimInstance:cmd(cmd)
-  self:_prevent_hanging()
-  -- TODO (sbadragan): check v:errmsg ?? and return output??
-  return self.api.nvim_exec2(cmd, { output = false })
-end
+    -- Properly exit Neovim. `pcall` avoids `channel closed by client` error.
+    -- Also wait for it to actually close. This reduces simultaneously opened
+    -- Neovim instances and CPU load (overall reducing flakey tests).
+    pcall(inst.cmd, 'silent! 0cquit')
+    vim.fn.jobwait({ job.id }, 1000)
 
-return NvimInstance
+    -- Close all used channels. Prevents `too many open files` type of errors.
+    pcall(vim.fn.chanclose, job.channel)
+    pcall(vim.fn.chanclose, job.id)
+
+    -- Remove file for address to reduce chance of "can't open file" errors, as
+    -- address uses temporary unique files
+    pcall(vim.fn.delete, job.address)
+
+    state.job = nil
+  end
+
+  -- NOTE: assignments below like `inst.api = vim.api` trick language server to have proper types
+
+  --- Sugar for `vim.api.xxx` API to be executed inside nvim instance
+  inst.api = vim.api
+  inst.api = setmetatable({}, {
+    __index = function(_, key)
+      return function(...)
+        local job = ensure_job_started()
+        return vim.rpcrequest(job.channel, key, ...)
+      end
+    end,
+  })
+
+  --- Variant of `api` functions called with `vim.rpcnotify`. Useful for making
+  --- blocking requests (like `getcharstr()`).
+  inst.api_notify = vim.api
+  inst.api_notify = setmetatable({}, {
+    __index = function(_, key)
+      return function(...)
+        local job = ensure_job_started()
+        return vim.rpcnotify(job.channel, key, ...)
+      end
+    end,
+  })
+
+  --- check if instance is blocked
+  --- i.e. it waits for user input and won't return from other call. Common causes are
+  --- active |hit-enter-prompt| (can mitigate by increasing prompt height to a bigger value)
+  --- or Operator-pending mode (can mitigate by exiting it)
+  ---@return boolean
+  function inst.is_blocked()
+    return inst.api.nvim_get_mode()['blocking']
+  end
+
+  function prevent_hanging()
+    if not inst.is_blocked() then
+      return
+    end
+
+    error('NvimInstance: Nvim is blocked waiting for input.')
+  end
+
+  --- runs vimscript in the context of the instance
+  ---@param cmd string
+  ---@return any result
+  inst.cmd = function(cmd)
+    prevent_hanging()
+
+    -- TODO (sbadragan): check v:errmsg ?? and return output??
+    return inst.api.nvim_exec2(cmd, { output = false })
+  end
+
+  --- Execute lua code and returns result (passed in string is prefixed with `return`).
+  --- Parameters (if any) are available as `...` inside the lua code chunk.
+  ---
+  --- examples:
+  --- local result = inst.lua('my_function(...)', { arg1, arg2 })
+  --- local result = inst.lua('MyPlugin.doThing()')
+  ---@param lua_code string lua code
+  ---@param args? table
+  ---@return any
+  inst.lua = function(lua_code, args)
+    prevent_hanging()
+    return inst.api.nvim_exec_lua('return ' .. lua_code, args or {})
+  end
+
+  --- Executes lua code without waiting for result. This is useful when making
+  --- blocking requests (like `getcharstr()`).
+  --- Parameters (if any) are available as `...` inside the lua code chunk.
+  ---
+  --- examples:
+  --- inst.lua_notify('getcharstr()')
+  ---@param lua_code string lua code
+  ---@param args? table
+  inst.lua_notify = function(lua_code, args)
+    prevent_hanging()
+    inst.api_notify.nvim_exec_lua(lua_code, args or {})
+  end
+
+  return inst
+end
